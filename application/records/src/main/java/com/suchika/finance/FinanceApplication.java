9beabbfa10c707d48300da4b53c9a2b0240d5d78
ketan <ketan.lkct@gmail.com>
2026-05-21 22:36:07 +0530

    Backend clean build

--- application/records/src/main/java/com/suchika/finance/FinanceApplication.java
+++ application/records/src/main/java/com/suchika/finance/FinanceApplication.java
@@ -3,7 +3,7 @@ package com.suchika.finance;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
-@QuarkusMain
+@QuarkusMain(name = "finance")
 public class FinanceApplication {
     public static void main(String... args) {
         Quarkus.run(args);
--- application/records/src/main/java/com/suchika/health/HealthApplication.java
+++ application/records/src/main/java/com/suchika/health/HealthApplication.java
@@ -3,7 +3,7 @@ package com.suchika.health;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
-@QuarkusMain
+@QuarkusMain(name = "health")
 public class HealthApplication {
     public static void main(String... args) {
         Quarkus.run(args);
