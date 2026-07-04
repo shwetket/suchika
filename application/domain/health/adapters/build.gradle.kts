38e30079e634c0f28144a4cf54ec5d793ad22fe4
ketan <ketan.lkct@gmail.com>
2026-07-04 12:02:18 +0530

    feat: ensure jacocoTestReport runs after quarkusBuild in build.gradle.kts files

--- application/domain/health/adapters/build.gradle.kts
+++ application/domain/health/adapters/build.gradle.kts
@@ -30,3 +30,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
+
+tasks.named("jacocoTestReport") {
+    mustRunAfter("quarkusBuild")
+}
